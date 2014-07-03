cc66c605d3b14549b73a405c2b224f32e5e19593
skaufman <sam@socialflow.com>
2014-07-03 15:43:30 +0000

    email attifier

---
+++ home_bin/attach_email.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+use strict;
+use warnings FATAL => 'all';
+use Email::Stuffer;
+
+my $host = `hostname`;
+chomp $host;
+my $from =  "$ENV{USER}\@$host";
+my $res = Email::Stuffer->from($from)->to($ARGV[0])->text_body($ARGV[1])
+    ->attach_file($ARGV[2])->send;
+warn $res;
