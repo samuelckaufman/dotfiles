f8c26b7cadcfd46bfc76c58f3d8b27ff070bfa26
skaufman <sam@socialflow.com>
2014-09-08 18:35:01 +0100

    gnupg plugiun

--- home_bin/attach_email.pl
+++ home_bin/attach_email.pl
@@ -2,10 +2,10 @@
 use strict;
 use warnings FATAL => 'all';
 use Email::Stuffer;
-
+use Devel::Dwarn;
 my $host = `hostname`;
 chomp $host;
 my $from =  "$ENV{USER}\@$host";
 my $res = Email::Stuffer->from($from)->to($ARGV[0])->text_body($ARGV[1])
     ->attach_file($ARGV[2])->send;
-warn $res;
+Dwarn $res;
